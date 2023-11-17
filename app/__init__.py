cd802c1feb52b398bd81b6af0017c6fa1f986d84
Marian <pidchashymaryan@gmail.com>
2023-11-17 16:01:53 +0200

    all tasks lab6

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-app.secret_key = b"Powerful secret"
-app.config['SECRET_KEY'] = 'Powerful secret'
+
+app.secret_key = b"secret"
 csrf = CSRFProtect(app)
 
 from app import views
